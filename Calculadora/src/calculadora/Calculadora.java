461bccead050e3877497e0d4f3386a2b9cbb82b5
hmorcar <hmoracarrasco@gmail.com>
2025-05-02 13:02:50 +0200

    Creación de la clase Calculadora con el método suma

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author 34685
+ */
+public class Calculadora {
+    
+    public double suma(double a, double b){
+        return a+b;
+    }
+    
+}
