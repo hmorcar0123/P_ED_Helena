c1e43f7cafc5500c21c0a62393c76c443de91c38
hmorcar <hmoracarrasco@gmail.com>
2025-05-02 14:03:43 +0200

    Creación del método resta

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -13,5 +13,8 @@ public class Calculadora {
     public double suma(double a, double b){
         return a+b;
     }
+    public double resta(double a, double b){
+        return a-b;
+    }
     
 }
